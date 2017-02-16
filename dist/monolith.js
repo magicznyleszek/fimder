5123be61ae01d138e83adc7bad37fa2941f8ef72
Leszek Pietrzak <smutnyleszek@gmail.com>
2017-02-16 09:48:08 +0100

    define angular app, fix small issues, uglify js on watch

--- dist/monolith.js
+++ dist/monolith.js
@@ -1,9 +1,30 @@
 ;
 
 (function () {
-  'use strict';
+    'use strict';
 
-  console.log('app.js');
+    angular.module('akabuskModule', []);
+
+    angular.module('akabuskModule').config(['$interpolateProvider', '$compileProvider', function ($interpolateProvider, $compileProvider) {
+        // unfortunately we can't use "{{ symbols }}" because Jekyll uses them
+        $interpolateProvider.startSymbol('[[');
+        $interpolateProvider.endSymbol(']]');
+
+        // We're disabling angular debug info - significant performance boost
+        $compileProvider.debugInfoEnabled(false);
+        // Don't look for directives in comments and classes (~10% boost)
+        $compileProvider.commentDirectivesEnabled(false);
+        $compileProvider.cssClassDirectivesEnabled(false);
+    }]);
+
+    // angular.module('akabuskModule').config(['$sceProvider', ($sceProvider) => {
+    //     $sceProvider.enabled(false);
+    // }]);
+
+    // angular.module('akabuskModule').run([
+    //     'fooBar',
+    //     angular.noop
+    // ]);
 })();
 ;
 
